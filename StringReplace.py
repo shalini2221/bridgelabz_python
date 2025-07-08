4ce5e20bc65784bb7c92d6efd23ecc16883eecba
Shalini Thorat <102426515+shalini2221@users.noreply.github.com>
2025-07-08 10:46:48 +0530

    Add files via upload

---
+++ StringReplace.py
@@ -0,0 +1,14 @@
+
+class StringReplace:
+	def ReplaceString(String[] args) 
+
+		// Initializing variables
+		Utility utility = new Utility()
+		template = "Hi <<UserName1>>, <<UserName2>> and <<UserName3>>"
+
+		System.out.print("Please enter three names: ")
+		String userName = utility.inputString()
+		
+		// Method declaration for replacing String
+		String outputString = utility.replaceString(userName1, userName2, userName3, template)
+		System.out.println(outputString)
