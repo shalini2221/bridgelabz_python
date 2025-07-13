6839cdefc88fcc9e1d2cecaa099ea1576053882d
Shalini Thorat <102426515+shalini2221@users.noreply.github.com>
2025-07-13 15:30:40 +0530

    Create IntOpt.py

---
+++ IntOpt.py
@@ -0,0 +1,20 @@
+def main():
+    # Taking input values
+    a = int(input("Enter value for a: "))
+    b = int(input("Enter value for b: "))
+    c = int(input("Enter value for c: "))
+
+    # Integer operations
+    result1 = a + b * c       # b * c first, then added to a
+    result2 = a * b + c       # a * b first, then added to c
+    result3 = c + a / b       # a / b first, then added to c (note: division gives float)
+    result4 = a % b + c       # a % b first, then added to c
+
+    # Printing results
+    print("a + b * c =", result1)
+    print("a * b + c =", result2)
+    print("c + a / b =", result3)
+    print("a % b + c =", result4)
+
+if __name__ == "__main__":
+    main()
